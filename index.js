07de42642b342e47e3fe0743def296536621eb02
root <root>
2018-07-19 12:56:23 +0900

    rename
    layoutConts -> contents

--- index.js
+++ index.js
@@ -9,27 +9,24 @@
  */
 mofron.layout.Grid = class extends mofron.Layout {
     
-    constructor (prm) {
+    constructor (po) {
         try {
             super();
             this.name('Grid');
-            this.prmOpt(
-                (('object' === typeof prm) && (undefined !== prm[0])) ?
-                    { 'value' : prm } : prm
-            );
+            this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    layoutConts (idx, tgt) {
+    contents (idx, tgt) {
         try {
             if ((null === this.value()) || (0 === this.value().length)) {
                 throw new Error('emply value');
             }
             if (0 === idx) {
-                this.target().style({
+                this.component().style({
                     'display'   : 'flex',
                     'flex-wrap' : 'wrap'
                 });
@@ -46,25 +43,6 @@ mofron.layout.Grid = class extends mofron.Layout {
         }
     }
     
-    value (val) {
-        try {
-            if (undefined === val) {
-                /* getter */
-                return (undefined === this.m_value) ? null : this.m_value;
-            }
-            /* setter */
-            if (('object' !== typeof val) || (undefined === val[0])) {
-                throw new Error('invalid parameter');
-            }
-            let sum = 0;
-            for (let idx_val in val) {
-                sum += val[idx_val];
-            }
-            this.m_value = val;
-        } catch (e) {
-            console.error(e.stack);
-            throw e;
-        }
-    }
 }
 module.exports = mofron.layout.Grid;
+/* end of file */
