4ac5a1736841115f73ec41d00a4e2449ee6964ba
simpart <simpart>
2019-07-03 18:18:16 +0000

    rename
    rate -> raito

--- index.js
+++ index.js
@@ -2,18 +2,15 @@
  * @file mofron-layout-grid/index.js
  * @author simpart
  */
+const mf = require("mofron");
 
-/**
- * @class mofron.layout.Grid
- * @brief Grid layout class
- */
 mofron.layout.Grid = class extends mofron.Layout {
     
     constructor (po) {
         try {
             super();
-            this.name('Grid');
-            this.prmMap('rate');
+            this.name("Grid");
+            this.prmMap("ratio");
             this.prmOpt(po);
         } catch (e) {
             console.error(e.stack);
@@ -25,33 +22,66 @@ mofron.layout.Grid = class extends mofron.Layout {
         try {
             if (0 === idx) {
                 this.component().style({
-                    'display'   : 'flex',
-                    'flex-wrap' : 'wrap'
+                    "display"   : "flex",
+                    "flex-wrap" : "wrap"
                 });
             }
-            tgt.width(this.rate()[(idx % this.rate().length)] + '%');
+            tgt.width(this.ratio()[(idx % this.ratio().length)] + '%');
         } catch (e) {
             console.error(e.stack);
             throw e;
         }
     }
     
-    rate (prm) {
+    ratio (prm) {
         try {
             if (undefined === prm) {
                 /* getter */
-                return (undefined === this.m_rate) ? [25,25,25,25] : this.m_rate;
+                return (undefined === this.m_ratio) ? [25,25,25,25] : this.m_ratio;
             }
             /* setter */
             if (false === Array.isArray(prm)) {
-                throw new Error('invalid parameter');
+                throw new Error("invalid parameter");
             }
             for (let ridx in prm) {
-                if ('number' !== typeof prm[ridx]) {
-                    throw new Error('invalid parameter');
+                if ("number" !== typeof prm[ridx]) {
+                    throw new Error("invalid parameter");
                 }
             }
-            this.m_rate = prm;
+            this.m_ratio = prm;
+        } catch (e) {
+            console.error(e.stack);
+            throw e;
+        }
+    }
+    
+    setDirctSize (prm, upd) {
+        try {
+            if (true !== Array.isArray(prm)) {
+                throw new Error("invalid parameter");
+            }
+            /* get sum */
+            let sum_num = 0;
+            for (let pidx in prm) {
+                sum_num += mf.func.getSize(prm[pidx]).value();
+            }
+            /* get ratio */
+            let ratio = [];
+            for (let pidx2 in prm) {
+                let buf = mf.func.getSize(prm[pidx2]).value()/sum_num;
+                ratio.push(Math.floor(buf*1000)/10);
+            }
+            this.m_ratio = ratio;
+            if (false === this.isInited()) {
+                return;
+            }
+            if (ratio.length !== this.component().child().length) {
+                throw new Error("invalid parameter");
+            }
+            let chd = this.component().child();
+            for (let cidx in chd) {
+                chd[cidx].width(prm[cidx]);
+            }
         } catch (e) {
             console.error(e.stack);
             throw e;
